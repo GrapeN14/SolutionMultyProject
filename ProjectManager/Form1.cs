680da2df63f453d143e27dc9f21bfb72adb4d658
ADMIN <ADMIN@DESKTOP-HRD3VOD>
2024-04-13 00:59:02 +0300

    Добавьте файлы проекта.

---
+++ ProjectManager/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProjectManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] resultBuffer = new string[1];
+            ProjectOne.Calendar.Main(resultBuffer);
+            MessageBox.Show(resultBuffer[0]);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            ProjectTwo.Form1 form1 = new ProjectTwo.Form1();
+            form1.Show();
+        }
+    }
+}
---
+++ ProjectOne/Calendar.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+namespace ProjectOne
+{
+    public class Calendar
+    {
+        public static void Main(string[] args)
+        {
+            DateTime now = GetCurrentDate();
+            Console.WriteLine($"Today's date is {now}");
+            if (args.Length != 0)
+            {
+                args[0] = $"Today's date is {now.ToShortDateString()}";
+                return;
+            }
+            Console.ReadKey();
+        }
+
+        static DateTime GetCurrentDate()
+        {
+            return DateTime.Now.Date;
+        }
+    }
+}
